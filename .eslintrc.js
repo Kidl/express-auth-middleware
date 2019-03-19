fb2065854e2a2dd3482cc77c20b63feca48ce379
Vladislav Sevostyanov <vladislav.sevostyanov@kidl.no>
2019-03-19 13:01:54 +0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,20 @@ module.exports = {
   'rules': {
     'no-use-before-define': ['error', {'functions': false}],
     'no-underscore-dangle': ['error', { 'allow': ['_id', '_queue'] }],
+    'one-var': 0,
+    'one-var-declaration-per-line': 0,
+    'prefer-destructuring': 0,
     'no-mixed-operators': 1,
     'no-console': 0,
     'no-param-reassign': 0,
     'consistent-return': 0,
     'no-shadow': 0,
     'max-len': 1,
+    'no-unused-vars': 1,
+    'import/newline-after-import': 0,
+  },
+  'env': {
+    'node': true,
+    'mocha': true
   },
 };
